071b27fdc0b8afbaec8adbca3c16c732793915d8
Almuktabar <m.almuktabar11@gmail.com>
2025-05-07 10:50:26 +0700

    update route icon utama + tombol back (awal)

---
+++ resources/views/dashboard/fasilitasrsudam.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="py-5 position-relative" style="min-height: 100vh;">
+        {{-- Language Switcher --}}
+
+        <div class="container text-center">
+            <h1>CONTENT NYA DI ISI DISINI YA GES</h1>
+        </div>
+    </section>
+@endsection
