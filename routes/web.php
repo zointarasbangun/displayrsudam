0f143d415ce911d09a1f8489aff30577d6514ba6
zoin <jointaras@gmail.com>
2025-05-14 17:06:29 +0700

    back end perubahan kelola pengguna dan marquee merge1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\DokterController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestimoniController;
